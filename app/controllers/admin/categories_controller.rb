a4ba78b4e68487a849c5e0c6556896d7de924873
Cheever Esler <ames.esler@gmail.com>
2022-12-15 20:08:12 -0400

    start admin/categories generate controller, add routes, start views

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,22 @@
+class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with  name: ENV["HTTP_BASIC_USERNAME"], password: ENV["HTTP_BASIC_PASSWORD"]
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :catregories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+  
+end
