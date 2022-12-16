b5bad9f54cc0f26e41eeb4f726cd3ca8d014ddae
Cheever Esler <ames.esler@gmail.com>
2022-12-16 15:17:53 -0400

    configure login routes and add links to nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+  
   get '/signup', to: 'users#new'
   post '/users', to: 'users#create'
 
